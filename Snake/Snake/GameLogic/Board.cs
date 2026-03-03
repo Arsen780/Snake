d15acbec9d9b93a7453dcbb88d1514cc5bebfdee
Arek Kot <132007504+Arsen780@users.noreply.github.com>
2026-03-03 18:26:18 +0100

    Piwerwszy commitg

---
+++ Snake/Snake/GameLogic/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Board
+    {
+
+        private const int width = 20;
+        private const int height = 20;
+
+
+
+        void CellState()
+        {
+
+        }
+
+
+    }
+}
---
+++ Snake/Snake/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Snake
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
